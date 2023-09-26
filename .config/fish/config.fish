4a93dcc78a9fb1739bc9a5f6bb9741232f1f2d75
Benoit Couloigner <benoit.couloigner@gmail.com>
2023-09-26 23:39:03 +0200

    update all

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,11 @@ abbr -a vim nvim
 abbr -a vi nvim
 abbr -a vimdiff 'nvim -d'
 
-if command -v exa > /dev/null
-	abbr -a l 'exa'
-	abbr -a ls 'exa'
-	abbr -a ll 'exa -l'
-	abbr -a lll 'exa -la'
+if command -v eza > /dev/null
+	abbr -a l 'eza'
+	abbr -a ls 'eza'
+	abbr -a ll 'eza -l'
+	abbr -a lll 'eza -la'
 else
 	abbr -a l 'ls'
 	abbr -a ll 'ls -l'
@@ -16,13 +16,14 @@ end
 abbr -a k kubectl
 abbr -a kns kubens
 abbr -a kct kubectx
-
+abbr -a nv neovide
+abbr -a gitgraph 'git log --oneline --graph --decorate --all'
 # Set envvars since we don't have .pam_environment
 setenv EDITOR nvim
 setenv BROWSER links
 setenv NAME "b.couloigner"
 setenv EMAIL b.couloigner@gmail.com
-set PATH $PATH ~/.cargo/bin ~/.local/bin/ ~/.npm-global/bin/
+set PATH $PATH ~/.cargo/bin ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/ ~/.local/bin/ ~/.npm-global/bin/ ~/.local/share/gem/ruby/3.0.0/bin
 
 
 # Fish should not add things to clipboard when killing
@@ -35,25 +36,20 @@ function fish_user_key_bindings
 		fzf_key_bindings
 	end
 end
-
+bind \cw kill-word
 function fish_prompt
-	set_color brblack
-	echo -n "["(date "+%H:%M")"] "
-	set_color yellow
-	echo -n (whoami)
 	set_color normal
-	echo -n "@"
-	echo -n (hostname -s)
 	if [ $PWD != $HOME ]
 		set_color brblack
-		echo -n ':'
 		set_color yellow
-		echo -n (basename $PWD)
+		echo -n $PWD | sed "s!$HOME!~!"
+else
+	echo -n "~/"
 	end
 	set_color green
 	printf '%s ' (__fish_git_prompt)
 	set_color red
-	echo -n '| '
+	echo -n '> '
 	set_color normal
 end
 
@@ -75,3 +71,11 @@ set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate ''
 set __fish_git_prompt_showupstream 'none'
 set -g fish_prompt_pwd_dir_length 3
+#  Start X at login
+if status --is-login
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx -- -keeptty
+  end
+end
+
+# starship init fish | source
